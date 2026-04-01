1e9b74c868400d2cdd9f4225438ca09bdb19f4ec
Ian McInerney <ian.s.mcinerney@ieee.org>
2026-04-01 19:15:38 +0100

    Make hop-over scaling GUI-independent and follow the schematic
    Previously, the hop-over and junction scalings required the eeschema
    frame to be loaded to compute the actually used scalings. This would not
    work for CLI exports. This changes it so that the scalings are now
    computed by the settings class independent of the GUI.
    
    Note that while this does remove the settings option for the junction
    size list, this was never exposed to the user and was in the frame, so
    it wasn't CLI-accessible.
    
    Fixes https://gitlab.com/kicad/code/kicad/-/work_items/23714

--- eeschema/sch_screen.cpp
+++ eeschema/sch_screen.cpp
@@ -915,7 +915,7 @@ void SCH_SCREEN::Plot( PLOTTER* aPlotter, const SCH_PLOT_OPTS& aPlotOpts, const 
 
     if( !aItems.empty() && aItems[0]->Schematic() )
     {
-        hopOverScale = aItems[0]->Schematic()->Settings().m_HopOverScale;
+        hopOverScale = aItems[0]->Schematic()->Settings().GetHopOverScale();
         defaultLineWidth = aItems[0]->Schematic()->Settings().m_DefaultLineWidth;
     }
 
