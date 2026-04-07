b76d2e33cfc46202b1313649da4c806b71690882
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2026-04-07 23:28:40 +0200

    Ifdef optional GAL trace call

--- common/gal/opengl/gpu_manager.cpp
+++ common/gal/opengl/gpu_manager.cpp
@@ -223,10 +223,12 @@ void GPU_CACHED_MANAGER::EndDrawing()
 
     cntDraw.Stop();
 
+#ifdef KICAD_GAL_PROFILE
     wxLogTrace( traceGalProfile,
               "Cached manager size: VBO size %u iranges %zu max elt size %u drawcalls %u",
               cached->AllItemsSize(), m_vranges.size(), m_indexBufMaxSize, drawCalls );
     wxLogTrace( traceGalProfile, "Timing: %s", cntDraw.to_string() );
+#endif
 
     glBindBuffer( GL_ARRAY_BUFFER, 0 );
     cached->ClearDirty();
